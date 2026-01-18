c0642c88348ada872e6d1590ca4a5cd83a94dc5f
siddhikhapare <siddhikhapare77@gmail.com>
2026-01-18 15:58:28 +0530

    frontend files modified

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   reactCompiler: true,
-  reactStrictMode: true,
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
